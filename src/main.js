4940a05803d0639217196d301e1cc8f99b8f934b
sej-dev <wndmstla1@gmail.com>
2021-11-15 15:07:04 +0900

    Feat: 루트 컴포넌트에 Vuex store 추가

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
+import store from "@/store";
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+
 new Vue({
   render: h => h(App),
+  store,
 }).$mount('#app')
 
 
