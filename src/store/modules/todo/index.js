7b82e45875f7df89ba4c8c4fecfb5a4efd8daf9c
sej-dev <wndmstla1@gmail.com>
2021-11-17 15:53:44 +0900

    Chore: todo store mutation 타입 사용 방법 변경

--- src/store/modules/todo/index.js
+++ src/store/modules/todo/index.js
@@ -1,24 +1,17 @@
 import TODO_STATUS from "@/constants/todo/todoStatus";
 import LIST_FILTER from "@/constants/todo/listFilter";
-import { 
-    UPDATE_TODO, UPDATE_TODO_BY_ID,
-    ADD_TODO, 
-    TOGGLE_TODO_BY_ID, TOGGLE_ALL_TODOS_STATUS,
-    DELETE_TODO_BY_ID, DELETE_ALL_COMPLETED_TODOS,
-    CHANGE_LIST_FILTER
-} from "@/store/modules/todo/mutation-types";
+import * as types from "@/store/modules/todo/mutation-types";
 
 const activePredicate = todo => todo.status === TODO_STATUS.ACTIVE;
 const completedPredicate = todo => todo.status === TODO_STATUS.COMPLETED;
 
 const state = () => ({
-    content: '',
     listFilter: LIST_FILTER.ALL,
     todos: [
         {id:0, content:'a', status:TODO_STATUS.COMPLETED},
         {id:1, content:'b', status:TODO_STATUS.ACTIVE},
     ],
-    _idx: 2
+    _sequence: 2
 });
 
 // 속성 유형 접근   - 캐싱 O
@@ -27,12 +20,14 @@ const getters = {
     getTodoById: (state) => (id) => {
         return state.todos.find(todo => todo.id === id);
     },
+
     getActiveTodos: (state) => () => {
         return state.todos.filter(activePredicate);
     },
     activeTodoCount: (state, getters) => {
         return getters.getActiveTodos().length;
     },
+    
     getCompletedTodos: (state) => () => {
         return state.todos.filter(completedPredicate);
     },
@@ -48,28 +43,25 @@ const actions = {
 
 // 동기 처리
 const mutations = {
-    [UPDATE_TODO](state, payload){
-        state.content = payload;
+    [types.ADD_TODO](state, payload){
+        const content = payload;
+        state.todos.push({ id : state._sequence++, status: TODO_STATUS.ACTIVE, content })
     },
-    [UPDATE_TODO_BY_ID](state, payload){
-        debugger
+    
+    [types.UPDATE_TODO_BY_ID](state, payload){
         const {id, content} = payload;
         const targetTodo = state.todos.find(todo => todo.id === id);
+        if(!targetTodo) return;
         targetTodo.content = content;
     },
 
-    [ADD_TODO](state, payload){
-        const content = payload;
-        state.todos.push({ id : state._idx++, status: TODO_STATUS.ACTIVE, content })
-    },
-    // TODO:  TodoList.vue에서 토글해도 자동으로 변경
-    [TOGGLE_TODO_BY_ID](state, payload){
+    [types.TOGGLE_TODO_BY_ID](state, payload){
         const id = id;
         const targetTodo = state.todos.find(todo => todo.id === id);
-        // TODO: 이렇게 작성해도 변경 사항이 반영되는지 확인
+        if(!targetTodo) return;
         targetTodo.status = targetTodo.status === TODO_STATUS.ACTIVE ? TODO_STATUS.COMPLETED : TODO_STATUS.ACTIVE;
     },
-    [TOGGLE_ALL_TODOS_STATUS](state){
+    [types.TOGGLE_ALL_TODOS_STATUS](state){
         const totalCount = state.todos.length;
         const completedTodos = state.todos.filter(completedPredicate);
         if(totalCount === completedTodos.length){
@@ -79,15 +71,15 @@ const mutations = {
         state.todos.map(todo => todo.status = TODO_STATUS.COMPLETED);
     },
 
-    [DELETE_TODO_BY_ID](state, payload){
+    [types.DELETE_TODO_BY_ID](state, payload){
         const id = payload;
         state.todos = state.todos.filter(todo => todo.id !== id);
     },
-    [DELETE_ALL_COMPLETED_TODOS](state){
+    [types.DELETE_ALL_COMPLETED_TODOS](state){
         state.todos = state.todos.filter(todo => todo.status === TODO_STATUS.ACTIVE)
     },
 
-    [CHANGE_LIST_FILTER](state, payload){
+    [types.CHANGE_LIST_FILTER](state, payload){
         state.listFilter = payload;
     }
 };
